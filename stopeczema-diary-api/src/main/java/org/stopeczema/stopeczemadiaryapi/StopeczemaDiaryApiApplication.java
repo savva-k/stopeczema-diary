b96fc383660106bf6f3b60cb118abf5bfcb71d50
Savva Kodeikin <savva.kodeikin@gmail.com>
2018-05-12 20:02:20 +0300

    Added a project for the API

---
+++ stopeczema-diary-api/src/main/java/org/stopeczema/stopeczemadiaryapi/StopeczemaDiaryApiApplication.java
@@ -0,0 +1,12 @@
+package org.stopeczema.stopeczemadiaryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StopeczemaDiaryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StopeczemaDiaryApiApplication.class, args);
+	}
+}
