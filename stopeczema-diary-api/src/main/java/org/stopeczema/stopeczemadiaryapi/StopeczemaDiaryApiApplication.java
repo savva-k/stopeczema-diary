20de99d7f5e10375269da7217b2fc3b321b9f846
Savva Kodeikin <savva.kodeikin@gmail.com>
2018-05-19 17:52:10 +0300

    Formatted some code

--- stopeczema-diary-api/src/main/java/org/stopeczema/stopeczemadiaryapi/StopeczemaDiaryApiApplication.java
+++ stopeczema-diary-api/src/main/java/org/stopeczema/stopeczemadiaryapi/StopeczemaDiaryApiApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StopeczemaDiaryApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(StopeczemaDiaryApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StopeczemaDiaryApiApplication.class, args);
+    }
 }
