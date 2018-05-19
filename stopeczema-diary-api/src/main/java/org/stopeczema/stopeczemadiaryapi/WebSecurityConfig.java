c74430c8cc83ab8523ef0e01fe2878fa720ac374
Savva Kodeikin <savva.kodeikin@gmail.com>
2018-05-20 00:16:38 +0300

    Security configuration changes
    Made the api available from another app,
    Marked the current configuration as development related,
    Made the dev profile active

--- stopeczema-diary-api/src/main/java/org/stopeczema/stopeczemadiaryapi/WebSecurityConfig.java
+++ stopeczema-diary-api/src/main/java/org/stopeczema/stopeczemadiaryapi/WebSecurityConfig.java
@@ -1,21 +1,47 @@
 package org.stopeczema.stopeczemadiaryapi;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 /**
  * @author Savva Kodeikin
  */
 @Configuration
+@Profile("dev")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.csrf().disable().cors().and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/chips/*").anonymous()
                 .antMatchers(HttpMethod.POST, "/chips").anonymous();
     }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*'
+        // when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
