1a0d9ec7c58512a54770875db644b9d8acb1d416
Savva Kodeikin <savva.kodeikin@gmail.com>
2018-05-21 16:33:47 +0300

    🦄 Renamed the unicornSvgPath to UnicornIcon

--- stopeczema-diary-front/src/scenes/Main/components/Sidebar/components/Unicorn/index.js
+++ stopeczema-diary-front/src/scenes/Main/components/Sidebar/components/Unicorn/index.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react'
 import ListItem from '@material-ui/core/ListItem'
 import SvgIcon from '@material-ui/core/SvgIcon'
 import ListItemText from '@material-ui/core/ListItemText'
-import UnicornPath from './unicornSvgPath'
+import UnicornPath from './UnicornIcon'
 export default function(props) {
   return (
     <Fragment>
