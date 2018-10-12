0baf82bee6408da85bbd248c27e5adf8e63ebba5
Savva Kodeikin <savva.kodeikin@gmail.com>
2018-10-13 01:51:05 +0300

    💪 Created a simple login page (#3) (#16)

--- stopeczema-diary-front/src/scenes/Sign/scenes/Login/index.js
+++ stopeczema-diary-front/src/scenes/Sign/scenes/Login/index.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import { withStyles, Paper, Grid, TextField, Button } from '@material-ui/core'
 
-export default () => {
-  return (
-    <div>
-      Login page
-    </div>
-  )
+const styles = theme => ({
+  loginForm: {
+    padding: '3px 15px 15px 15px',
+  },
+
+  loginFormGridRow: {
+    textAlign: 'center',
+    width: '50%',
+    marginBottom: '10px',
+  },
+
+  textField: {
+    width: '100%',
+  },
+})
+
+class Login extends React.Component {
+  render() {
+    const { classes } = this.props
+    return (
+      <Paper className={classes.loginForm}>
+        <Grid container direction="column" alignItems="center" justify="space-between">
+          <Grid item className={classes.loginFormGridRow}>
+            <h1>Login</h1>
+          </Grid>
+          <Grid item className={classes.loginFormGridRow}>
+            <TextField id="login" label="Login" className={classes.textField} />
+          </Grid>
+          <Grid item className={classes.loginFormGridRow}>
+            <TextField
+              id="password"
+              label="Password"
+              className={classes.textField}
+              type="password"
+            />
+          </Grid>
+          <Grid item className={classes.loginFormGridRow}>
+            <Button variant="contained" color="primary" className={classes.button}>
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+    )
+  }
 }
+
+export default withStyles(styles)(Login)
--- stopeczema-diary-front/src/scenes/Sign/scenes/Registration/index.js
+++ stopeczema-diary-front/src/scenes/Sign/scenes/Registration/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default () => {
-  return (
-    <div>
-      Registration of new users
-    </div>
-  )
+class Registration extends React.Component {
+  render() {
+    return <div>Login</div>
+  }
 }
+
+export default Registration
