2e3ace10709c63f1e7f5eeb14aa69d840462c757
Savva Kodeikin <savva.kodeikin@gmail.com>
2018-10-17 00:08:28 +0300

    Added an endpoint for receiving the current user

--- stopeczema-diary-api/src/main/java/org/stopeczema/stopeczemadiaryapi/controllers/UsersRestController.java
+++ stopeczema-diary-api/src/main/java/org/stopeczema/stopeczemadiaryapi/controllers/UsersRestController.java
@@ -1,5 +1,7 @@
 package org.stopeczema.stopeczemadiaryapi.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,9 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.stopeczema.stopeczemadiaryapi.dto.UserDTO;
+import org.stopeczema.stopeczemadiaryapi.dto.json.wrapper.UserWrapper;
 import org.stopeczema.stopeczemadiaryapi.services.UserService;
-
-import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author Savva Kodeikin
@@ -24,6 +25,7 @@ import java.util.concurrent.ThreadLocalRandom;
 public class UsersRestController {
 
     private static final String SUCCESSFULLY_CREATED = "UserEntity has been successfully created";
+    private static final ObjectMapper jacksonMapper = new ObjectMapper();
 
     @Autowired
     private UserService userService;
@@ -42,9 +44,9 @@ public class UsersRestController {
         return new ResponseEntity<>(SUCCESSFULLY_CREATED, HttpStatus.CREATED);
     }
 
-    @GetMapping(value = "/check", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> isLoggedIn() {
-        String answer = String.format("{ \"loggedIn\": %s }", ThreadLocalRandom.current().nextBoolean());
-        return new ResponseEntity<String>(answer, HttpStatus.OK);
+    @GetMapping(value = "/current", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getCurrentUser() throws JsonProcessingException {
+        UserWrapper userWrapper = new UserWrapper(userService.getCurrentUser());
+        return new ResponseEntity<>(jacksonMapper.writeValueAsString(userWrapper), HttpStatus.OK);
     }
 }
