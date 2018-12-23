7ec434d74934ed7ab58cd532eccafc5ea7579b84
Savva Kodeikin <savva.kodeikin@gmail.com>
2018-12-24 01:24:43 +0300

    Played with React Hooks

--- stopeczema-diary-front/src/scenes/Main/scenes/Unicorn/index.js
+++ stopeczema-diary-front/src/scenes/Main/scenes/Unicorn/index.js
@@ -1,5 +1,15 @@
 import React from 'react'
+import { useState } from 'react'
+import { Button } from '@material-ui/core'
 
 export default props => {
-  return <h1>UNICORN</h1>
+  const [count, setCount] = useState(0)
+
+  return (
+    <React.Fragment>
+      <h1>Unicorn</h1>
+      <h2>Testing React hooks: count: {count}</h2>
+      <Button onClick={() => setCount(count + 1)}>Add</Button>
+    </React.Fragment>
+  )
 }
